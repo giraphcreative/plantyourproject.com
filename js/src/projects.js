c1da7708f5b544b1c189444b6eea93a7a32ccc93
James Pederson <james@jpederson.com>
2017-04-08 04:36:13 -0500

    Take better care to adjust image sizes when loading and resizing the page.

--- js/src/projects.js
+++ js/src/projects.js
@@ -1,9 +1,38 @@
 
 
 var set_project_photo_height = function(){
+	
+	// get project photo width
 	var project_width = $('.project-photo:first').width();
-	console.log( project_width );
+
+	// set heights on the project photo div
 	$('.project-photo').height( project_width );
+
+	// loop through all the project photos and set interior image heights.
+	$('.project-photo').each(function(){
+		
+		// get photo img width
+		var $photo = $(this).find('img');
+		var photo_height = $photo.height();
+		var photo_width = $photo.width();
+
+		// if the photo height is less than the project width, we'll resize it to fill the space.
+		if ( photo_height < photo_width ) {
+
+			// set height of interior img to height of project if it's not already that way
+			$photo.height( project_width );
+
+			// get the photo width
+			var photo_overflow = $photo.width() - project_width;
+
+			// set the margin on the photo so it is 'centered'.
+			$photo.css( 'margin-left', -( photo_overflow / 2 ) );
+
+		} else {
+			$photo.width( project_width );
+		}
+
+	});
 };
 
 $(function(){
